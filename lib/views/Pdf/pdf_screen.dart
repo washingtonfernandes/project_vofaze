69b4c6f4726e30942a62e54498b2e2fb0f2e21d5
washingtonfernandes <washington.fernandes@rede.ulbra.br>
2024-05-01 17:32:52 -0300

    pdf

--- lib/views/Pdf/pdf_screen.dart
+++ lib/views/Pdf/pdf_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:typed_data';
-
+import 'package:share/share.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
